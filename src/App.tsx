24b351042da4c79f8d0eb52a05b255f7b994faf1
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2023-01-11 23:40:43 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import { useState } from "react"
-import { Tweet } from "./components/Tweet"
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import usePersistedState from '../src/utils/usePersistedState'
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+
+
+import  React, { useContext,
+                 useState } 
+                 from 'react';
+
+//import './App.css';
+import GlobalStyle from "./styles/global";
 import {AppRoutes} from "./Routes"
-import './App.css';
+//import Form from "./components/Form";
+import Input from "./components/Input";
+import Header from "./components/header/index";
+
+
 
 function App() {
-  return <AppRoutes />
+
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+  const toogleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }
+  return (
+    <ThemeProvider theme ={ theme }>
+      <div>
+        <GlobalStyle />
+        <Header toggleTheme={ toogleTheme }/>
+        <AppRoutes />
+      </div>
+    </ThemeProvider>
+  );
 /*   const [tweets, setTweets] = useState<string[]>([
       'Tweet1',
       'Tweet2',
---
+++ src/components/Logo.tsx
@@ -0,0 +1,20 @@
+import CycloneIcon from '@mui/icons-material/Cyclone';
+import styled from 'styled-components';
+
+export default function Logo() {
+    /* const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+ */
+    return (
+        <div>
+            <CycloneIcon />
+        </div>
+        );
+}
+
